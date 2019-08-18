e5bbe45e8a741d214b5da779f0b8e24f53224509
Shankar <shnara@outlook.com>
2019-08-17 23:28:43 -0700

    Move questions to a new json file.

--- src/app/questionBank.service.ts
+++ src/app/questionBank.service.ts
@@ -1,75 +1,17 @@
 import { Helper } from './common.helper';
 import { QuestionModel } from './questionModel.component';
+import { Injectable } from '@angular/core';
 
+// Load the questions.
+import questionsRaw from "../assets/questions.json";
+
+@Injectable()
 export class QuestionBankService {
   totalNumberOfQuestions: number;
   questions: QuestionModel[];
 
   constructor() {
-    this.questions = [
-      {
-        id: 1,
-        question: 'What does SLPR stand for?',
-        answer: 'Slow, Look, Press, Roll',
-        options: [
-          'Slow, Look, Press, Roll',
-          'Slow, Lean, Power, Ride',
-          'See, Lean, Push, Race'
-        ],
-        explanation: '',
-        usersAnswer: ''
-      },
-      {
-        id: 2,
-        question: 'What is your favorite bike?',
-        answer: 'Kawasaki',
-        options: [
-          'Kawasaki',
-          'Yamaha',
-          'Honda'
-        ],
-        explanation: '',
-        usersAnswer: ''
-      }
-      ,
-      {
-        id: 3,
-        question: 'What is your most important role in reducing risk?',
-        answer: 'Time + Space + SEE',
-        options: [
-          'Time + Space + SEE',
-          'Dont ride a bike.',
-          'Buy insurance.'
-        ],
-        explanation: '',
-        usersAnswer: ''
-      },
-      {
-        id: 4,
-        question: 'What is the friction zone?',
-        answer: 'Some zone',
-        options: [
-          'Some zone',
-          'Dont know',
-          'Ice cream'
-        ],
-        explanation: '',
-        usersAnswer: ''
-      },
-      {
-        id: 5,
-        question: 'What are escape routes?',
-        answer: 'Routes for escaping',
-        options: [
-          'Routes for escaping',
-          'A movie',
-          'Ice cream'
-        ],
-        explanation: '',
-        usersAnswer: ''
-      }
-    ];
-
+    this.questions = questionsRaw;
     this.totalNumberOfQuestions = this.questions.length;
   }
 
